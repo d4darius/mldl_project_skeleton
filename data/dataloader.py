43b58a416d6b0dfb66ed572b31092465b8e09105
Dario Gosmar <119061629+d4darius@users.noreply.github.com>
2025-03-19 16:30:12 +0100

    Dataset file fix

--- data/dataloader.py
+++ data/dataloader.py
@@ -10,8 +10,8 @@ transform = T.Compose([
 
 # root/{class}/x001.jpg
 
-tiny_imagenet_dataset_train = ImageFolder(root='../dataset/tiny-imagenet/tiny-imagenet-200/train', transform=transform)
-tiny_imagenet_dataset_val = ImageFolder(root='../dataset/tiny-imagenet/tiny-imagenet-200/val', transform=transform)
+tiny_imagenet_dataset_train = ImageFolder(root='/dataset/tiny-imagenet/tiny-imagenet-200/train', transform=transform)
+tiny_imagenet_dataset_val = ImageFolder(root='/dataset/tiny-imagenet/tiny-imagenet-200/val', transform=transform)
 
 train_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_train, batch_size=32, shuffle=True)
 val_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_val, batch_size=32, shuffle=False)
--- utils/download_dataset.py
+++ utils/download_dataset.py
@@ -16,11 +16,11 @@ if response.status_code == 200:
         zip_file.extractall('/dataset')
     print('Download and extraction complete!')
 
-with open('../dataset/tiny-imagenet/tiny-imagenet-200/val/val_annotations.txt') as f:
+with open('/dataset/tiny-imagenet/tiny-imagenet-200/val/val_annotations.txt') as f:
     for line in f:
         fn, cls, *_ = line.split('\t')
-        os.makedirs(f'../dataset/tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
+        os.makedirs(f'/dataset/tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
 
-        shutil.copyfile(f'../dataset/tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'../dataset/tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
+        shutil.copyfile(f'.dataset/tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'/dataset/tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
 
-shutil.rmtree('../dataset/tiny-imagenet/tiny-imagenet-200/val/images')
+shutil.rmtree('/dataset/tiny-imagenet/tiny-imagenet-200/val/images')

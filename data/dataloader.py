429b8beeee51f4dc91cef7e8d0dcce71e6675700
Dario Gosmar <119061629+d4darius@users.noreply.github.com>
2025-03-20 11:21:47 +0100

    Adding Debugging prints in dataloader

--- data/dataloader.py
+++ data/dataloader.py
@@ -14,5 +14,8 @@ dataset_path = os.path.join(os.getcwd(), 'dataset')
 tiny_imagenet_dataset_train = ImageFolder(root=os.path.join(dataset_path, 'tiny-imagenet-200', 'train'), transform=transform)
 tiny_imagenet_dataset_val = ImageFolder(root=os.path.join(dataset_path, 'tiny-imagenet-200', 'val'), transform=transform)
 
+print("Loading the dataset...")
+print(f"Training samples: {len(tiny_imagenet_dataset_train)}")
 train_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_train, batch_size=32, shuffle=True)
+print(f"Validation samples: {len(tiny_imagenet_dataset_val)}")
 val_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_val, batch_size=32, shuffle=False)
